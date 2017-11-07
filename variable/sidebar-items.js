63ebea34bc1e1bbae8355f51d9d7735a532daf35
Vlado Boza <bozavlado@gmail.com>
2017-11-07 22:07:25 +0100

    docs

---
+++ variable/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["classification_error",""],["cross_entropy_with_softmax",""],["elem_times",""],["named_alias",""],["plus",""],["reduce_sum_all",""],["squared_error",""],["tanh",""],["times",""]],"struct":[["ParameterInitializer",""],["Variable",""]]});
