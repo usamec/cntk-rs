2cc3297892c98b3ba467618a404d59c19c166b76
Vlado Boza <bozavlado@gmail.com>
2017-11-07 23:20:23 +0100

    0.0.3

--- cntk/ops/sidebar-items.js
+++ cntk/ops/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["classification_error",""],["cross_entropy_with_softmax",""],["elem_times",""],["named_alias",""],["plus",""],["reduce_sum_all",""],["squared_error",""],["tanh",""],["times",""]]});
+initSidebarItems({"fn":[["classification_error",""],["cross_entropy_with_softmax",""],["elem_times",""],["last",""],["minus",""],["named_alias",""],["past_value",""],["plus",""],["reduce_sum_all",""],["sigmoid",""],["squared_error",""],["tanh",""],["times",""]]});
