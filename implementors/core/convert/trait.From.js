5b816243bc72827f724fbefbc37e5e6b00bef108
Vlado Boza <bozavlado@gmail.com>
2017-11-17 17:10:50 +0100

    version 0.1.0

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cntk"] = ["impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;<a class=\"struct\" href=\"cntk/struct.Function.html\" title=\"struct cntk::Function\">Function</a>&gt;&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;T&gt; for <a class=\"struct\" href=\"cntk/struct.Variable.html\" title=\"struct cntk::Variable\">Variable</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;&amp;'a <a class=\"struct\" href=\"cntk/struct.Variable.html\" title=\"struct cntk::Variable\">Variable</a>&gt; for <a class=\"struct\" href=\"cntk/struct.Variable.html\" title=\"struct cntk::Variable\">Variable</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
